f7c9e619cf178bf0891fd16c4ed47e7df9c8c06b
viktor.toth <viktor.toth@r-das.sk>
2021-03-17 19:15:13 +0100

    jarne upratovanie

---
+++ main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+
+//Standard libraries
+#include <string.h>
+
+//Waspmote libraries
+#include <WaspSensorEvent_v30.h>   
+#include <WaspWIFI_PRO.h>
+#include <WaspFrame.h>
+#include <BME280.h>
+#include <WaspAES.h>
+
+//Project libraries
+#include "wifiManager.h"
+
+#endif//MAIN_H_
---
+++ wifiManager.h
@@ -0,0 +1,6 @@
+#ifndef WIFIMANAGER_H_
+#define WIFIMANAGER_H_
+
+#include "main.h"
+
+#endif//WIFIMANAGER_H_
